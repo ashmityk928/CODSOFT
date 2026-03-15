c44f6fc3bed76711276624c04b73d30614680cce
Ashmit Yadav <162987045+ashmityk928@users.noreply.github.com>
2026-03-15 19:41:37 +0530

    Add number guessing game implementation

---
+++ Task1_Number_Game.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Number Guessing Game");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too high!");
+            } 
+            else if (guess < number) {
+                System.out.println("Too low!");
+            } 
+            else {
+                System.out.println("Correct! You guessed in " + attempts + " attempts.");
+            }
+
+        } while (guess != number);
+
+        sc.close();
+    }
+}
